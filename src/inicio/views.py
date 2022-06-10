9f894ac804d15c8a16af3533026d8ac831ed25b2
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-09 23:00:12 -0500

    Creacion de la app inicio y se registra un url

---
+++ src/inicio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def myHomeView(*args, **kwargs):
+    return HttpResponse('<h1>HOLA MUNDO DESDE DJANDO EN VIEWS.PY</h1>')
