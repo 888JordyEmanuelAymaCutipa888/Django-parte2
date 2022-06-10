862f014aab3e0970f8a508e07eabca0f814d50f9
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-09 22:40:17 -0500

    Cambiado el modelo y alicando la migracion correspondiente

--- src/destinosTuristicos/models.py
+++ src/destinosTuristicos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class DestinoTuristico(models.Model):
-    nombreCiudad = models.CharField(max_length = 100)
+    nombreCiudad = models.TextField()
     descripcionCiudad = models.CharField(max_length = 100)
     precioTour = models.IntegerField()#(max_digits=3)
     ofertaTour = models.BooleanField()
