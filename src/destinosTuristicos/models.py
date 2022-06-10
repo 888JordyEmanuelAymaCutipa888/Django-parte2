680e3a29b492c7d6b904ddd23be524ba75608e57
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-09 22:18:59 -0500

    Se corrige el error en la tabla

--- src/destinosTuristicos/models.py
+++ src/destinosTuristicos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class DestinoTuristico(models.Model):
-    nombreCiudad = models.charField(max_length = 100)
-    descripcionCiudad = models.charField(max_length = 100)
+    nombreCiudad = models.CharField(max_length = 100)
+    descripcionCiudad = models.CharField(max_length = 100)
     imagenCiudad = models.ImageField()
     precioTour = models.IntegerField()#(max_digits=3)
     ofertaTour = models.booleanField()
