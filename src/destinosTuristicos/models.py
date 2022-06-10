40b71f50130344f9f76125347ff5aab0666ecaa0
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-09 22:21:08 -0500

    Se elimina el campo de imagen la tabla pues aun no se tiene la imagen

--- src/destinosTuristicos/models.py
+++ src/destinosTuristicos/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class DestinoTuristico(models.Model):
     nombreCiudad = models.CharField(max_length = 100)
     descripcionCiudad = models.CharField(max_length = 100)
-    imagenCiudad = models.ImageField()
     precioTour = models.IntegerField()#(max_digits=3)
     ofertaTour = models.BooleanField()
