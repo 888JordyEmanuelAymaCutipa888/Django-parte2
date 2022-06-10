a63ad85029f0167d8d933a27a4b622dd71a2cb2e
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-09 22:11:19 -0500

    Creacion de la tabla DestinoTuristico

--- src/destinosTuristicos/models.py
+++ src/destinosTuristicos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class DestinoTuristico(models.Model):
+    nombreCiudad = models.charField(max_length = 100)
+    descripcionCiudad = models.charField(max_length = 100)
+    imagenCiudad = models.ImageField()
+    precioTour = models.IntegerField()#(max_digits=3)
+    ofertaTour = models.booleanField()
